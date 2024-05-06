16284b8d8a64f600a81d5fc84b236746cb8376d4
alicjapisk <106108825+alicjapisk@users.noreply.github.com>
2024-05-06 14:28:51 +0200

    changed bgHero and customized screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-import Image from "next/image";
+"use client";
 import Main from "./components/Main";
-import HeroBg from "../../public/assets/HeroBg.png";
+import HeroBg from "../../public/assets/HeroBg3.webp";
+import HeroBgMobile from "../../public/assets/HeroBgMobile.webp";
+
+import { useLayoutEffect, useState } from "react";
 
 export default function Home() {
+  const [isLGScreen, setIsLGScreen] = useState(true);
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setIsLGScreen(window.innerWidth >= 1135);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <main>
       <div
-        style={{ backgroundImage: `url(${HeroBg.src})` }}
-        className="bg-no-repeat bg-center bg-cover"
+        style={{
+          backgroundImage: `url(${isLGScreen ? HeroBg.src : HeroBgMobile.src})`
+        }}
+        className="bg-no-repeat bg-cover min-h-[100vh] bg-white"
       >
         <Main />
       </div>
